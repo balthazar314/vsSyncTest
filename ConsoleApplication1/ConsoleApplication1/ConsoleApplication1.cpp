80689be04bda0e9584f75d5391c7cbd8707cd1b1
Philip Stuckey <pstuc002.public@gmail.com>
2016-04-21 20:47:36 -0400

    First commit, trying to figure this all out

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("hello world\njust a test\n");
+	getchar();
+	return 0;
+}
+
